7a9fbcbebf3a48211f6bccac68140532a8e4ddec
Dave Novick <davenovick@hotmail.com>
2017-08-04 11:55:58 -0700

    Added command line parsing, foreach, and String.Format.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("You are a foo foo!");
+            Console.WriteLine("Here are my args!");
+
+            int i = 0;
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(String.Format("Argument #{0} = {1}",i, arg));
+                i++;
+            }
+
+            if (args[0] == "chinese")
+            {
+                Console.WriteLine("I like Chinese food. You know that it's true.");
+            }
+
+            Console.WriteLine("Bye bye.");
 
         }
     }
